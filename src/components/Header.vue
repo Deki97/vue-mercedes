00020501e2939bee5e0793eec2e8ac67e552afad
Deki97 <dejan97@libero.it>
2022-01-03 21:08:43 +0100

    Header done

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo-left">
+                <img src="https://car-brand-names.com/wp-content/uploads/2015/05/Mercedes-symbol-720x640.jpg" alt="Mercedes Logo">
+            </div>
+
+            <div class="logo-right">
+                <img src="https://www.mercedes-benzcaribbean.com/assets/themes/mb-caribbean/media/stock_images/word-logo2b.png" alt="Mercedes Motto">
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+header {
+    background-color: $page-primary-color;
+    
+    .container {
+        display: flex;
+        align-items: center;
+    
+        .logo-left {
+            width: 100px;
+        }
+
+        .logo-right {
+            width: 200px;
+            margin-left: 40px;
+        }
+    }
+}
+</style>
---
+++ src/components/SingleMercedes.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="single-mercedes">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
